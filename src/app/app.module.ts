7aba2958f9f66e11272570cbb705bd5946a37dae
mijim <migueljimenezbenajes@gmail.com>
2018-03-20 18:12:14 +0100

    Dependencies, login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './components/log-in/log-in.component';
 
+import {AngularFireModule} from 'angularfire2';
+import {environment} from '../environments/environment';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AuthService} from './auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:  {
+    apiKey: "AIzaSyAkfk4jUnypxk84wE1PTFGJCjOV4eDDtXg",
+    authDomain: "giveme5-9a35a.firebaseapp.com",
+    databaseURL: "https://giveme5-9a35a.firebaseio.com",
+    projectId: "giveme5-9a35a",
+    storageBucket: "giveme5-9a35a.appspot.com",
+    messagingSenderId: "978254767810"
+  }
 };
