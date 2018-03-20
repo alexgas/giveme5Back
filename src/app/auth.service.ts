53bb7a6aa185057bce78fe61e2b28a2363b53eef
mijim <migueljimenezbenajes@gmail.com>
2018-03-20 18:31:34 +0100

    dependencies

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,46 @@
 import { Injectable } from '@angular/core';
 
 import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
+import * as firebase from 'firebase';
 
 import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class AuthService {
-  user: Observable<firebase.User>:
+  user: Observable<firebase.User>;
 
-  constructor() { }
+  constructor(private firebaseAuth: AngularFireAuth) {
+    this.user = firebaseAuth.authState;
+  }
+
+  signup(email: string, password: string) {
+    this.firebaseAuth
+        .auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(value => {
+          console.log('Usuario registrado correctamente!');
+        })
+        .catch(err => {
+          console.log('Error: ',err.message);
+        });
+  }
+
+  login(email: string, password: string){
+    this.firebaseAuth
+        .auth
+        .signInWithEmailAndPassword(email, password)
+        .then(value => {
+          console.log('Usuario loggeado correctamente');
+        })
+        .catch(err => {
+          console.log('Error: ',err.message);
+        });
+  }
+
+  logout() {
+    this.firebaseAuth
+      .auth
+      .signOut();
+  }
 
 }
